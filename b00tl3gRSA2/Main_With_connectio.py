683e3b2a654915cfce73d55a3628b087762bfaba
Zed <96841607+Z3D-9@users.noreply.github.com>
2022-07-15 10:08:26 -0700

    Create Main_With_connectio.py

---
+++ b00tl3gRSA2/Main_With_connectio.py
@@ -0,0 +1,16 @@
+from Crypto.Util.number import long_to_bytes
+
+from pwn import * # pip install pwntools
+
+conn=connect('jupiter.challenges.picoctf.org',57464)
+
+
+c=int(conn.recvuntil(b'\n').decode().strip()[2:])
+n=int(conn.recvuntil(b'\n').decode().strip()[2:])
+e=int(conn.recvuntil(b'\n').decode().strip()[2:])
+
+for i in range(10000000):
+    m=pow(c,i,n)
+    if b'pic' in long_to_bytes(m):
+        print(long_to_bytes(m))
+        break
